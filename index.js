8a7d160c8b54b599be2b63ba881c95d65394d1ef
Joseph Martin <josephmartin1350@gmail.com>
2021-11-22 10:26:28 -0700

    commit tree 0b528ee3deb1ca0bf57640ac68b43ce116bb7742

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 8;
+let num4 = 180000000000;
+let mod = num3 / num2;
+
+let max = Math.max(5, 10, 15, 20);
